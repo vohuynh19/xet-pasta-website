b5048b5780fc1b4f191e30d5cb6e338b87b6f484
VoHuynh <huynh.vo@unifiedpost.com>
2023-10-12 00:40:19 +0700

    feat: test

---
+++ src/app/test/page.tsx
@@ -0,0 +1,17 @@
+import { signIn } from "next-auth/react";
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="xetpasta://home"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click me
+        </a>
+      </div>
+    </main>
+  );
+}
